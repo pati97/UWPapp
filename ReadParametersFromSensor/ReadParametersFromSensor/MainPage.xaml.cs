3d7b31a38e939ea36817762899560d5f1b204196
Patrycja Polek <patipolek1@gmail.com>
2019-10-03 12:02:36 +0200

    Added sending device to cloud with weather parameters.

--- ReadParametersFromSensor/ReadParametersFromSensor/MainPage.xaml.cs
+++ ReadParametersFromSensor/ReadParametersFromSensor/MainPage.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,46 +17,93 @@ namespace ReadParametersFromSensor
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        DispatcherTimer timer;
-        BuildAzure.IoT.Adafruit.BME280.BME280Sensor bME280;
-
+        private DispatcherTimer timer;
+        private BuildAzure.IoT.Adafruit.BME280.BME280Sensor bME280;
+        private static DeviceClient serviceDeviceClient;
+        
         const float seaLevelPressure = 1022.00f;
-
+        private static string connectionString = "HostName=IoTHubMember.azure-devices.net;DeviceId=MyRpi;SharedAccessKey=F4CptKs4Pn9bc1oi43GXsqFtgsKAFnEd2q5XA53rKRk=";
+        private static string deviceID = "MyRpi";
+        private int messageId = 1;
+       
         public MainPage()
         {
-           this.InitializeComponent();
+            this.InitializeComponent();
+
+            serviceDeviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Amqp);
+            //s_deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
         }
 
+        private async Task SendDeviceToCloudMessageAsync(double temp, float humidity, float pressure, float altitude)
+        {
+            var text = "HELLO";
+
+            while (true)
+            {
+                var textcurrent = text + "+";
+                var telemetryDataPoint = new {
+                messageId = messageId++,
+                deviceId = deviceID,
+                temperature = temp,
+                humidity = humidity,
+                pressure = pressure
+                };
+
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                //Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                try
+                {
+                    await serviceDeviceClient.SendEventAsync(message);
+                    Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine("Error Message: {0}",ex.Message);
+                }
+                Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+            }
+        }
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
             bME280 = new BuildAzure.IoT.Adafruit.BME280.BME280Sensor();
             await bME280.Initialize();
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(5);
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(5)
+            };
             timer.Tick += TimerTick;
 
             timer.Start();
         }
+
         private async void TimerTick(object sender, object e)
         {
             try
             {
-                var temperature = await bME280.ReadTemperature();
-                var humidity = await bME280.ReadHumidity();
-                var pressure = await bME280.ReadPressure() / 100;
-                var altitude = await bME280.ReadAltitude(seaLevelPressure);
-
-                Debug.WriteLine("Temperature: {0} deg C", temperature);
-                Debug.WriteLine("Humidity: {0} %", humidity);
-                Debug.WriteLine("Pressure: {0} hPa", pressure);
-                Debug.WriteLine("Altitude: {0} m", altitude);
+                await ReadParameters();  
             }
             catch
             {
                 Debug.WriteLine("Cannot read value from sensor...");
             }
+        }
+        private async Task ReadParameters()
+        {
+            var temperature = await bME280.ReadTemperature();
+            var humidity = await bME280.ReadHumidity();
+            var pressure = await bME280.ReadPressure() / 100;
+            var altitude = await bME280.ReadAltitude(seaLevelPressure);
+
+            Debug.WriteLine("Temperature: {0} deg C", temperature);
+            Debug.WriteLine("Humidity: {0} %", humidity);
+            Debug.WriteLine("Pressure: {0} hPa", pressure);
+            Debug.WriteLine("Altitude: {0} m", altitude);
+            await SendDeviceToCloudMessageAsync(temperature, humidity, pressure, altitude);
         }
     }
 }
---
+++ ReadParametersFromSensor/ReadParametersFromSensor/SendDataToIotHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Client;
+
+namespace ReadParametersFromSensor
+{
+    public class SendDataToIotHub
+    {
+        private static DeviceClient deviceClient;
+        //private readonly static string connectionString = "HostName=IoTHubMember.azure-devices.net;DeviceId=MyRpi;SharedAccessKey=F4CptKs4Pn9bc1oi43GXsqFtgsKAFnEd2q5XA53rKRk=";
+        //private readonly static string iotHubUri = "IoTHubMember.azure-devices.net";
+        //private readonly static string deviceId = "MyRpi";
+        //private readonly static string deviceKey = "F4CptKs4Pn9bc1oi43GXsqFtgsKAFnEd2q5XA53rKRk=";
+
+        public static async void SendDataToAzure()
+        {
+            var text = "HELLO";
+            //deviceClient = DeviceClient.Create(iotHubUri, AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), TransportType.Http1);
+            //deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Amqp);
+            while (true)
+            {
+                var textcurrent = text + "+";
+                var telemetryDataPoint = new { textcurrent };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                //Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await deviceClient.SendEventAsync(message);
+                Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
